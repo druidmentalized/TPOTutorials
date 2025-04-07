c18ba7e625049a188cadc8e3b8e54bad119526bb
druidmentalized <dmitry.barmuta@gmail.com>
2025-04-07 21:28:19 +0200

    Converter html page added

--- tpo05/src/main/java/org/project/controllers/ConversionController.java
+++ tpo05/src/main/java/org/project/controllers/ConversionController.java
@@ -1,5 +1,62 @@
+/*
 package org.project.controllers;
 
-public class ConversionController {
+import org.project.services.ConversionService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigInteger;
+
+@Controller
+public class ConversionController {
+    private static final char[] DIGITS = (
+            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=[]{},.<>/?;:'\"\\|`~"
+    ).toCharArray();
+
+    private static final int BIN = 2;
+    private static final int OCT = 8;
+    private static final int HEX = 16;
+
+    private final ConversionService conversionService;
+
+    public ConversionController(ConversionService conversionService) {
+        this.conversionService = conversionService;
+    }
+
+    @PutMapping("/")
+    public void convertValue(
+            @RequestParam(value = "value") String value,
+            @RequestParam(value = "fromBase") int fromBase,
+            @RequestParam(value = "toBase") int toBase) {
+
+        // To decimal at first => to others from it
+        BigInteger decimal = toDecimal(value, fromBase);
+
+        // Needed one
+        String required = fromDecimal(decimal, toBase);
+
+        // Additional info
+        String binary = fromDecimal(decimal, BIN);
+        String octal = fromDecimal(decimal, OCT);
+        String hex = fromDecimal(decimal, HEX);
+    }
+
+    // Helper method for conversion
+
+    private BigInteger toDecimal(String value, int fromBase) {
+
+    }
+
+    private String fromDecimal(BigInteger value, int toBase) {
+
+    }
+
+    private int valueOfChar(char c) {
+        for (int i = 0; i < DIGITS.length; i++) {
+            if (DIGITS[i] == c) return i;
+        }
+        return -1;
+    }
 }
+*/
--- tpo05/src/main/java/org/project/services/ConversionService.java
+++ tpo05/src/main/java/org/project/services/ConversionService.java
@@ -1,5 +1,8 @@
 package org.project.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ConversionService {
 
 }
