fc9f85df848a1b093bf140cd068d23fedf72a05c
druidmentalized <dmitry.barmuta@gmail.com>
2025-04-16 23:46:02 +0200

    New module for tpo07 created

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "TPOTutorials"
-include("tpo04-books", "tpo04-blog", "tpo05", "tpo06")
+include("tpo04-books", "tpo04-blog", "tpo05", "tpo06", "tpo07")
---
+++ tpo07/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.project"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.google.googlejavaformat:google-java-format:1.26.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
